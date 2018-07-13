2c06272469961233721907d7c2744b20f0486cb3
sampanna pokhrel <sampanna99@gmail.com>
2018-07-12 22:16:08 -0400

    custom validation a lil tricky  not hard

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,11 +10,12 @@ export class AppComponent implements OnInit {
   genders = ['male', 'female'];
 
   signupForm: FormGroup;
+  forbiddenUsernames = ['Chris', 'Anna'];
 
   ngOnInit(){
     this.signupForm = new FormGroup({
       'userData': new FormGroup({
-      'username': new FormControl(null, Validators.required),
+      'username': new FormControl(null, [Validators.required, this.forbiddenNames.bind(this)]),
       'email': new FormControl(null, [Validators.required, Validators.email])
       }),
       'gender': new FormControl('male'),
@@ -27,5 +28,12 @@ onSubmit(){
 onAddHobby(){
   const control = new FormControl(null, Validators.required);
   (<FormArray>this.signupForm.get('hobbies')).push(control);
+}
+
+forbiddenNames(control: FormControl): {[s: string]: boolean} {
+  if(this.forbiddenUsernames.indexOf(control.value) !== -1) {
+    return {'name is forbidden': true}
+  }
+  return null;
 }
 }
